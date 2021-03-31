a9ecb83b9e67d0a80b976f0bdb6c59aff7d038e0
Rob Payne <robert.payne@austincc.edu>
2021-03-31 13:51:30 -0500

    Start

---
+++ src/main/java/com/example/demo/ReactPart2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactPart2Application.class, args);
+	}
+
+}
